af91c55278d0b89edaee97d2c0658328031601c5
D. Alfred Ostrowski <115842612+dalfredostrowski@users.noreply.github.com>
2023-12-15 18:07:48 -0500

    Update vesting.ts

--- vesting.ts
+++ vesting.ts
@@ -5,7 +5,6 @@ dotenv.config();
 import { NODE_URL, FAUCET_URL } from "./common";
 
 import { AptosClient, AptosAccount, FaucetClient, } from "aptos";
-
 //URLS
 //const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
 //const FAUCET_URL = process.env.APTOS_FAUCET_URL || "https://faucet.devnet.aptoslabs.com";
